554ea5bff64f001c4b4d741b219aa58b74755a0f
Wascharapong Phummon <rickyharvey.w@gmail.com>
2024-05-10 13:36:42 +0700

    feat: Objects Assignment

--- ex-3.js
+++ ex-3.js
@@ -13,7 +13,7 @@ const employee = {
   },
 };
 
-console.log("Employee Name: " + employees.name);
+console.log("Employee Name: " + employee.name);
 console.log("Employee Age: " + employee.age);
 console.log(
   "Employee Address: " +
